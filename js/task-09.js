a05363ec42d08be4a4d32eaaa08e73cd7a80aeac
Amadimm <amadeuszrolecki@gmail.com>
2023-04-24 23:14:11 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
-function getRandomHexColor() {
+
+
+const bodyColor = document.body;
+const buttonChangeColor = document.querySelector(`.change-color`);
+const color =  document.querySelector(`.color`);
+
+
+function getRandomHexColor () {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+
+  buttonChangeColor.addEventListener(`click`,  () => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+    color.textContent = getRandomHexColor() ;
+  });
