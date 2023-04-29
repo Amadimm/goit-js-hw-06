e7e5dd458972e9ad7b3ce18eace494180853905b
Amadimm <amadeuszrolecki@gmail.com>
2023-04-29 22:26:17 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,9 @@
 const textInput = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
+
+text.style.fontSize = textInput.value + "px";
+
 function changeSize() {
   text.style.fontSize = `${textInput.value}px`;
 }
