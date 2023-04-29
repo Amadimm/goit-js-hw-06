0cab56c64341baa9a02a02ec3a153974d2ae80e7
Amadimm <amadeuszrolecki@gmail.com>
2023-04-29 22:18:17 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,4 +3,8 @@ const outputText = document.querySelector("#name-output");
 
 inputText.addEventListener("input", (event) => {
   outputText.textContent = event.currentTarget.value;
+
+  if (outputText.textContent == "") {
+    outputText.textContent = "Anonymous";
+  }
 });
